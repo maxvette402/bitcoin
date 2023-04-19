d33313101d88c3937fe249d3538a314ef56ce0ec
Cory Fields <cory-nospam-@coryfields.com>
2023-04-19 16:11:45 -0400

    depends: switch libevent to CMake

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -3,15 +3,17 @@ $(package)_version=fc9bfd210d289d1565cf03e3d9d2e85f50f5b144
 $(package)_download_path=https://github.com/libevent/libevent/archive
 $(package)_file_name=$($(package)_version).tar.gz
 $(package)_sha256_hash=5457907404df32dd35436004f3b94e64b3cdf1320c274411803404933732788c
+$(package)_build_subdir=build
 
 # When building for Windows, we set _WIN32_WINNT to target the same Windows
 # version as we do in configure. Due to quirks in libevents build system, this
 # is also required to enable support for ipv6. See #19375.
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared --disable-openssl --disable-libevent-regress --disable-samples
-  $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
-  $(package)_config_opts_release=--disable-debug-mode
+  $(package)_config_opts=-DEVENT__DISABLE_BENCHMARK=ON -DEVENT__DISABLE_OPENSSL=ON
+  $(package)_config_opts+=-DEVENT__DISABLE_SAMPLES=ON -DEVENT__DISABLE_REGRESS=ON
+  $(package)_config_opts+=-DBUILD_TESTING=OFF -DEVENT__LIBRARY_TYPE=STATIC
   $(package)_cppflags_mingw32=-D_WIN32_WINNT=0x0601
+  $(package)_config_opts_debug+=-DEVENT__DISABLE_DEBUG_MODE=ON
 
   ifeq ($(NO_HARDEN),)
   $(package)_cppflags+=-D_FORTIFY_SOURCE=3
@@ -19,8 +21,7 @@ define $(package)_set_vars
 endef
 
 define $(package)_config_cmds
-  ./autogen.sh && \
-  $($(package)_autoconf)
+  $($(package)_cmake) -S .. -B .
 endef
 
 define $(package)_build_cmds
@@ -32,7 +33,6 @@ define $(package)_stage_cmds
 endef
 
 define $(package)_postprocess_cmds
-  rm lib/*.la && \
   rm include/ev*.h && \
   rm include/event2/*_compat.h
 endef
